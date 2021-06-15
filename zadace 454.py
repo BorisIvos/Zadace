c83611a3002edd0bfa66df011ce0d60b142b71de
BorisIvos <85962855+BorisIvos@users.noreply.github.com>
2021-06-15 21:53:18 +0200

    Add files via upload

--- zadace 454.py
+++ zadace 454.py
@@ -1,1 +1,15 @@
+import re
+string=input("Unesite e-mail: ")
 
+regex="[a-z].[a-z][@][f][p][m][o][z].[s][u][m].[b][a]$"
+match=re.findall(regex,string)
+if match:
+    print("Uspješno ste unijeli vašu e-mail adresu")
+else:
+    print("e-mail nije validan")
+string2=input("unesite eduld: ")
+regex2="[a-z][@][s][u][m].[b][a]$"
+match2=re.findall(regex2,string2)
+if match2:
+    print("Uspješno ste unijeli vaš edul")
+else:print("Vaš edul nije validan")
