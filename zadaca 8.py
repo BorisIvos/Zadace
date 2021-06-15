c90d408aa841467e3194ae7c0cb8f65f2abfab89
BorisIvos <85962855+BorisIvos@users.noreply.github.com>
2021-06-15 22:13:43 +0200

    Add files via upload

--- zadaca 8.py
+++ zadaca 8.py
@@ -1,1 +1,11 @@
+def prva(ime):
+  return ("Pozdrav",{ime},"!")
+print(prva("Boris"))
 
+druga=lambda x: ("Dobrodosao" ,{x}, "!")
+print(druga("Boris"))
+
+def treca():
+  return druga("Boris")
+print(treca())
+© 2021 GitHub, Inc.
